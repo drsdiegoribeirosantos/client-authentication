c5fca452297edfcd5f5deada62aa2add32689c49
Diego Ribeiro <diego24gt@hotmail.com>
2025-07-09 20:15:39 -0300

    chore: setup inicial com Spring Boot 3.5.3, Java 17

---
+++ src/test/java/com/java/clientauthentication/ClientAuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.clientauthentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientAuthenticationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
